2b92c13ad7cdbd9e106824ef155e3486b27d4013
Ari Archer <ari@ari.lt>
2024-04-28 19:56:46 +0300

    Improvements: Use absolute paths, use /social path
    * Removed useless whitespace in some files
    * Replace all relative paths with absolute paths
    * Move /social.html to /social/index.html so you could just use /social
    
    Signed-off-by: Ari Archer <ari@ari.lt>

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", (el) => {
     [...Array(10).keys()].forEach(() => {
         img = document.createElement("img");
-        img.src = "./images/what.svg";
+        img.src = "/images/what.svg";
         img.style.marginTop = (Math.random() * (window.innerHeight*0.95 - img.offsetHeight)) + "px";
         img.style.marginLeft = (Math.random() * (window.innerWidth - img.offsetWidth)) + "px";
         document.getElementById("backpic").appendChild(img);
@@ -12,14 +12,14 @@ window.addEventListener("load", (el) => {
     document.addEventListener('mousemove', function(event) {
         var mouseX = event.clientX;
         var mouseY = event.clientY;
-    
+
         circle.style.left = mouseX - circle.offsetWidth / 2 + 'px';
         circle.style.top = mouseY - circle.offsetHeight / 2 + 'px';
     });
 
     Array.from(document.getElementsByClassName("contact")).forEach((contact) => {
         contact.addEventListener("click", (ev) => {
-            const currentUrl = window.location.href; 
+            const currentUrl = window.location.href;
             switch (contact.id) {
                 case "email":
                     window.location.href = "mailto:rhssh@rhssh.xyz&body=you should kill yourself, NOW!";
@@ -27,7 +27,7 @@ window.addEventListener("load", (el) => {
                         window.location.href = currentUrl;
                     }, 500);
                     break;
-                
+
                 case "twitter":
                     window.location.href = "https://twitter.com/nordrtp";
                     break;
@@ -39,7 +39,7 @@ window.addEventListener("load", (el) => {
                 case "matrix":
                     window.location.href = "https://matrix.to/#/@t1nklas:matrix.org";
                     break;
-            } 
-        }) 
+            }
+        })
     })
 })
